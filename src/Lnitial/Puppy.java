16d5a1e6fc00e1a4f08565d239773e77fee5cc52
Small Rui <603049583@qq.com>
2018-03-13 14:40:48 +0800

    Java 修饰符

---
+++ src/Lnitial/Puppy.java
@@ -0,0 +1,18 @@
+package Lnitial;
+
+public class Puppy {
+    int puppyAge;
+    public Puppy(String name){
+        // 这个构造器仅有一个参数：name
+        System.out.println("小狗的名字是 : " + name );
+    }
+
+    public void setAge( int age ){
+        puppyAge = age;
+    }
+
+    public int getAge( ){
+        System.out.println("小狗的年龄为 : " + puppyAge );
+        return puppyAge;
+    }
+}
--- src/Lnitial/carpenterClassMethod.java
+++ src/Lnitial/carpenterClassMethod.java
@@ -1,11 +1,11 @@
 package Lnitial;
 
 public class carpenterClassMethod {
+
     public static void main(String[] args)
     {
         Employee empOne = new Employee("RUNOOB1");
         Employee empTwo = new Employee("RUNOOB2");
-
         // 调用这两个对象的成员方法
         empOne.empAge(26);
         empOne.empDesignation("高级程序员");
@@ -16,6 +16,19 @@ public class carpenterClassMethod {
         empTwo.empDesignation("菜鸟程序员");
         empTwo.empSalary(500);
         empTwo.printEmployee();
+        /* 创建对象 */
+        Puppy myPuppy = new Puppy( "tommy" );
+        /* 通过方法来设定age */
+        myPuppy.setAge( 2 );
+        /* 调用另一个方法获取age */
+        myPuppy.getAge( );
+        /*你也可以像下面这样访问成员变量 */
+        System.out.println("变量值 : " + myPuppy.puppyAge );
+        PrimitiveTypeTest primitive = new PrimitiveTypeTest();
+        primitive.PrimitivePrint();
+        ClassTypeConvert typeConvert = new ClassTypeConvert();
+        typeConvert.TypeConvert();
+        typeConvert.StrongTypeConvert();
     }
 }
 
