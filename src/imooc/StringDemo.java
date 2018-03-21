c36da6617ceb0c62dde7d975739d51f68ad945e9
Small Rui <603049583@qq.com>
2018-03-22 00:47:50 +0800

    Java流程控制之循环结构1

---
+++ src/imooc/StringDemo.java
@@ -0,0 +1,12 @@
+package imooc;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String s1 = "";
+        System.out.println(s1);
+        String s2 = "Trust";
+        System.out.println(s2);
+        String s3 = "\u005d\u005f";
+        System.out.println(s3);
+    }
+}
---
+++ src/imooc/VarDemo.java
@@ -0,0 +1,15 @@
+package imooc;
+
+public class VarDemo {
+    public static void main(String[] args) {
+        int x,y;
+        x = 5;
+        y = 3;
+
+        System.out.println(x);
+        System.out.println(y);
+        double d = 1.23E5;
+        float f = 1.234e3f;
+        System.out.println("d="+d+"/"+"f="+f);
+    }
+}
