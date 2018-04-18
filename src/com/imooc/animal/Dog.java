5fa1cdf9cab01df2afb526cbfa754dae5484c555
Small Rui <603049583@qq.com>
2018-04-18 22:49:00 +0800

    方法的重写

--- src/com/imooc/animal/Dog.java
+++ src/com/imooc/animal/Dog.java
@@ -14,4 +14,22 @@ public class Dog extends Animal {
     public void sleep(){
         System.out.println(this.getName()+"现在"+this.getMonth()+"大了,他在睡觉");
     }
+    /**
+     * 方法重载：
+     * 1、同一个类中
+     * 2、方法名相同，参数列表不同
+     * 3、方法返回值、访问修饰符任意
+     *
+     * 方法重写：
+     * 1、有继承关系的子类中
+     * 2、方法名相同，参数列表相同（参数顺序、个数、类型），方法返回值相同
+     * 3、访问修饰符？
+     * 4、与方法的参数名无关？
+     * */
+    private String sleep(String name){
+        return "";
+    }
+    public void eat(){
+        System.out.println(this.getName()+"最近没有食欲～");
+    }
 }
